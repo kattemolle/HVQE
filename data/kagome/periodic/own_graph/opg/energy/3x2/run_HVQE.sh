2f6957a271a468ebd6a2be4f158d4b1260181c82
Joris Lisa GPU <jorisatkattemolledotcom>
2021-01-12 15:14:27 +0100

    Sim results

--- data/kagome/periodic/own_graph/opg/energy/3x2/run_HVQE.sh
+++ data/kagome/periodic/own_graph/opg/energy/3x2/run_HVQE.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 #SBATCH -N 1
-#SBATCH -n 4
+#SBATCH -n 2
 #SBATCH -t 5-00:00:00
 #SBATCH -p gpu_shared
 #SBATCH --mem=1GB
 #SBATCH --gres=gpu:1
 
-for n_par in {468..468}
-do
-	 python3 ~/HVQE/HVQE.py $PWD $n_par 1  
-done
+python3 ~/HVQE/HVQE.py $PWD 612 1  
+
--- data/kagome/periodic/own_graph/ops/energy/3x2/run_HVQE.sh
+++ data/kagome/periodic/own_graph/ops/energy/3x2/run_HVQE.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 #SBATCH -N 1
-#SBATCH -n 4
+#SBATCH -n 2
 #SBATCH -t 5-00:00:00
 #SBATCH -p gpu_shared
 #SBATCH --mem=1GB
 #SBATCH --gres=gpu:1
 
-for n_par in {12..240..12}
-do
-	 python3 ~/HVQE/HVQE.py $PWD $n_par 3  
-done
+python3 ~/HVQE/HVQE.py $PWD 276 3  

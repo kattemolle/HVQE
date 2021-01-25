b4d8e3450f3dfb310981c8adf5d589b5e75df379
Joris Lisa GPU <jorisatkattemolledotcom>
2021-01-25 22:09:04 +0100

    sims

---
+++ data/chain/periodic/own_graph/ops/energy/18/gs.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#SBATCH -N 1
+#SBATCH -n 2
+#SBATCH -t 10:00:00
+#SBATCH -p gpu_shared
+#SBATCH --mem=1GB
+#SBATCH --gres=gpu:1
+
+python3 ~/HVQE/ground_state.py $PWD 2
---
+++ data/chain/periodic/own_graph/ops/energy/22/run_HVQE.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH -N 1
+#SBATCH -n 2
+#SBATCH -t 12:00:00
+#SBATCH -p gpu_shared
+#SBATCH --mem=1GB
+#SBATCH --gres=gpu:1
+
+for n_par in {26..26..2}
+do
+	 python3 ~/HVQE/HVQE.py $PWD $n_par 11  
+done

5b5fd7126c7ed9dbea3e2e2f9fdf36d08f12f8b1
Joris lisa_CPU <jorisatkattemolledotcom>
2021-02-05 11:11:18 +0100

    sims

--- data/kagome/periodic/own_graph/opg/energy/2x2/run_VQE.sh
+++ data/kagome/periodic/own_graph/opg/energy/2x2/run_VQE.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 #SBATCH -N 1
-#SBATCH -n 4
-#SBATCH -t 1-00:00:00
-#SBATCH -p fat_soil_shared
-#SBATCH --mem=50MB
+##SBATCH -n 4
+#SBATCH -t 5-00:00:00
+##SBATCH -p fat_soil_shared
+##SBATCH --mem=50MB
 ##SBATCH --gres=gpu:1
 
-for npar in {48..480..24}
+for mult in {1..5}
 do
-python3 ~/HVQE/HVQE.py $PWD $npar 1 
+python3 ~/HVQE/HVQE.py $PWD 264 1 & 
+sleep .1
 done
+wait
--- data/kagome/periodic/own_graph/opl/energy/2x2/run_VQE.sh
+++ data/kagome/periodic/own_graph/opl/energy/2x2/run_VQE.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 #SBATCH -N 1
-#SBATCH -n 4
-#SBATCH -t 1-00:00:00
-#SBATCH -p fat_soil_shared
-#SBATCH --mem=50MB
+##SBATCH -n 4
+#SBATCH -t 5-00:00:00
+##SBATCH -p fat_soil_shared
+##SBATCH --mem=50MB
 ##SBATCH --gres=gpu:1
 
-for npar in {8..80..4}
+for np in {1..5}
 do
-python3 ~/HVQE/HVQE.py $PWD $npar 6 
+python3 ~/HVQE/HVQE.py $PWD 60 6 &
+sleep 0.1
 done
+wait

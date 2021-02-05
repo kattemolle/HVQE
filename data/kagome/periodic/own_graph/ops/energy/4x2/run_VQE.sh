2c4b553abc3300d2f32a435834bb4c10f46575f6
Joris Obelix <jorisatkattemolledotcom>
2021-02-05 11:07:19 +0100

    sims

--- data/kagome/periodic/own_graph/ops/energy/4x2/run_VQE.sh
+++ data/kagome/periodic/own_graph/ops/energy/4x2/run_VQE.sh
@@ -2,9 +2,9 @@
 #SBATCH -N 1
 #SBATCH -n 4
 #SBATCH -t 90-00:00:00
-#SBATCH --mem=120GB
+#SBATCH --mem=200GB
 
-for i in {12..10000..12}
+for i in {12..1000..12}
 do
-python3 ~/HVQE/HVQE.py $PWD $i 4 --dump_interval 64
+python3 ~/HVQE/HVQE.py $PWD $i 4
 done

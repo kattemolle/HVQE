7e62d18a7ecae6bb99786a46af906e11bc799783
Joris Lisa Quantum <jorisstkattemolledotcom>
2021-09-28 19:29:55 +0200

    sims

---
+++ data/triangular/periodic/own_graph/opg/energy/3x3/run_HVQE.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH -N 1
+#SBATCH -n 4
+#SBATCH -t 00:01:00
+#SBATCH -p fat_soil_shared
+#SBATCH --mem=10GB
+
+
+python3 ~/HVQE/qem.py
+
+
+#for mult in {1..10}
+#do
+#python3 ~/HVQE/HVQE.py $PWD 1332 1 &
+#sleep .1
+#done
+#wait
+

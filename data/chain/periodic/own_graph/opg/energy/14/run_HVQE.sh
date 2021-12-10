81cac33f6a3a3248674ced369f3cfdb063149d21
Joris Lisa Quantum <jorisstkattemolledotcom>
2021-12-10 07:54:24 +0100

    sims

--- data/chain/periodic/own_graph/opg/energy/14/run_HVQE.sh
+++ data/chain/periodic/own_graph/opg/energy/14/run_HVQE.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 #SBATCH -N 1
-#SBATCH -n 2
+#SBATCH -n 6
 #SBATCH -t 1-00:00:00
-#SBATCH -p gpu_shared
-#SBATCH --mem=1GB
+#SBATCH -p fat_soil_shared
+#SBATCH --mem=3GB
 #SBATCH --gres=gpu:1
 
+python3 ~/HVQE_/HVQE.py $PWD 70 1 &
+python3 ~/HVQE_/HVQE.py $PWD 84 1 &
+python3 ~/HVQE_/HVQE.py $PWD 98 1 &
 
-python3 ~/HVQE/HVQE.py $PWD 56 1

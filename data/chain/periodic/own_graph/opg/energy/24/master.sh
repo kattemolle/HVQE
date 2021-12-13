9460a85b04bf782fc7c32765bb962d42d67ca609
Joris Lisa Quantum <jorisstkattemolledotcom>
2021-12-13 10:27:51 +0100

    sims

--- data/chain/periodic/own_graph/opg/energy/24/master.sh
+++ data/chain/periodic/own_graph/opg/energy/24/master.sh
@@ -1,9 +1,9 @@
-for i in {1..32}
-    do for n_par in $(seq 0 24 264)
-       do sed -i '$ d' run_HVQE.sh
-          echo "python3 ~/HVQE_/HVQE.py $PWD $n_par 1" >> run_HVQE.sh
-          sbatch run_HVQE.sh
-          sleep 0.1
-       done
+for n_par in $(seq 0 24 216)
+    do for i in {1..16}
+        do sed -i '$ d' run_HVQE.sh
+            echo "python3 ~/HVQE_/HVQE.py $PWD $n_par 1" >> run_HVQE.sh
+            sbatch run_HVQE.sh
+            sleep 0.1
+        done
     done
 done     
